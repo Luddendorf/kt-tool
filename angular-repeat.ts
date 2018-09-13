3c7a6a06a1f0c6b9d2589be10b28063ab0eb9795
Luddendorf <luddendorf2@yahoo.com>
2018-09-13 23:24:07 +0300

    Update angular-repeat.ts

--- angular-repeat.ts
+++ angular-repeat.ts
@@ -18,6 +18,7 @@ export class AppComponent {
 import { Component, OnInit } from '@angular/core';
 
 import { Receipt } from '../receipt';
+import { RECEIPTS } from '../mock.receipts';
 
 @Component({
   selector: 'app-receipts',
@@ -25,12 +26,18 @@ import { Receipt } from '../receipt';
   styleUrls: ['./receipts.component.css']
   
   export class ReceiptsComponent implements OnInit {
-  
+  /*
   receipt: Receipt = {
     id: 1,
     name: 'Smashed Potato';
-   };
+   }; */
+   selectedReceipt: Receipt;
    
+   receipts = RECEIPTS;
+    
+   onSelect(receipt: Receipt): void {
+     this.selectedReceipt = receipt;   
+}
    
   constructor() {}
   
@@ -41,16 +48,31 @@ import { Receipt } from '../receipt';
   }
   
  //  receipts.component.html
- <h2>{{ receipt.name | uppercase }} Details</h2>
- <div><span>id: </span>{{ receipt.id }}</div>
- <div><span>name: </span>{{ receipt.name }}</div>
+<div *ngIf="selectedReceipt">
+  
+ <h2>{{ selectedReceipt.name | uppercase }} Details</h2>
+ <div><span>id: </span>{{ selectedReceipt.id }}</div>
+ <div><span>name: </span>{{ selectedReceipt.name }}</div>
  
  // after refactoring:
  <div>
    <label>
-     <input [(ngModel)]="receipt.name" placeholder="name" />
+     <input [(ngModel)]="selectedReceipt.name" placeholder="name" />
    </label>
  </div>
+
+</div>
+
+// refactoring 2:
+<h2>My Receipts</h2>
+<ul class="receipts">
+    <li *ngFor="let receipt of receipts"
+        (click)="onSelect(receipt)"
+        [class.selected]="receipt === selectedReceipt">
+      <span class="badge">{{receipt.id}}</span> {receipt.name}}
+    </li>
+</ul>
+
 
 //// receipt.ts
 export class Receipt {
@@ -73,7 +95,17 @@ declarations: [
   AppComponent,
   ReceiptsComponent
   ],
-  
+ 
+  //  mock-receipts.ts
+  import { Receipt } from './receipt';
+
+export const RECEIPTS: Receipt[] = [
+    { id: 11, name: 'Pizza' },
+    { id: 12, name: 'Burger' },
+    { id: 13, name: 'Fried chicken' },
+    { id: 14, name: 'Coca-cola' },
+    { id: 15, name: 'Pasta' }
+    ];
 
 
 
