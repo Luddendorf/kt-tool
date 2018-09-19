ace2ce5b3b5e09e7b6ca5302458b23ad79a8072c
Luddendorf <luddendorf2@yahoo.com>
2018-09-19 12:29:00 +0300

    Create receipt-book.ts

---
+++ receipt-book.ts
@@ -0,0 +1,12 @@
+/* three.js / pixi.js / d3.js */
+/*
+[ngStyle]="{'background-color': getColor(person.country)}"
+[ngStyle]="{'font-size.px':24}"
+
+[ngClass]="{
+'text-success':person.country === 'UK',
+'text-primary':person.country === 'USA',
+'text-danger':person.country === 'HK'}"
+
+*ngFor="let logItem of log; let i = index;"
+[ngClass]="{'white-text': i >= 4}" */
