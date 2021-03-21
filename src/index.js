969ee0289b0798bb545b8940f033873e8e23eed3
Filipichev <d.filipichev@andersenlab.com>
2021-03-21 23:54:21 +0200

    Lesson 2 task.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import './main.css';
 import styles from './index.module.css';
 import "./sass/main-second.scss";
 
-// const h1 = document.querySelector('h1');
-// h1.classList.add(styles.h1);
-
 async function print() {
   // Here we are using dynamic import
   const { greet } = await import('./greet');
@@ -14,7 +11,6 @@ async function print() {
 
 print();
 
-let res = document.querySelector('.seasons').firstElementChild.innerHTML = 'MOUSE';
 
 
 
