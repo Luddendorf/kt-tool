a08551cc5888d39449a7171146bb0d580df99db8
unknown <staslukyanenko1995@gmail.com>
2021-03-26 15:52:41 +0300

    fix: script

--- src/script.js
+++ src/script.js
@@ -1,26 +1,14 @@
-"use strict";
+"use strict"
 
-let mainForm = document.forms.main;
-let iframeForm = document.forms.iframe;
-let mainInput = document.querySelector("#main-input");
-let iframeInput = document.querySelector("#iframe-input");
-let mainSubmit = document.querySelector('#main-submit');
-let checkbox = document.querySelector(".checkbox");
-let radio = document.querySelectorAll("input[type=radio]");
-
-
-function getData() {
-   document.querySelector('textarea').innerHTML = mainInput.value
-}
-
-mainSubmit.addEventListener('click', getData);
+const checkbox = document.querySelector(".checkbox")
+const radio = document.querySelectorAll(".radio")
 
 checkbox.addEventListener("click", function () {
- radio.forEach((item)=>{
-    if (checkbox.checked) {
-        item.disabled = true
-    }else{
-        item.disabled = false
-    }
- })
+    radio.forEach((item)=>{
+        if (checkbox.checked) {
+            item.disabled = true
+        }else{
+            item.disabled = false
+        }
+    })
 });
