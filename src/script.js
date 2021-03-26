3d641eea51c5a7482571ff82c72dd470941c8f58
unknown <staslukyanenko1995@gmail.com>
2021-03-26 18:53:53 +0300

    add: dispatch form

--- src/script.js
+++ src/script.js
@@ -1,16 +1,28 @@
-window.addEventListener('DOMContentLoaded', () => {
 
-    const checkbox = document.querySelector('.checkbox')
-    const radio = document.querySelectorAll('.radio')
+const checkbox = document.querySelector('.checkbox')
+const radio = document.querySelectorAll('.radio')
 
-    checkbox.addEventListener('change', event => {
-        const target = event.target
-        radio.forEach(item => {
-            if (target.checked) {
-                item.disabled = true
-            } else {
-                item.disabled = false
-            }
-        })      
-    })
+checkbox.addEventListener('change', event => {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+        radio.forEach(item => item.disabled = true);
+    } else {
+        radio.forEach(item => item.disabled = false);
+    }
 })
+
+const submitButton = document.querySelector('.submit-button');
+
+submitButton.addEventListener('click', event => {
+  const name = document.querySelector('.name').value;
+  localStorage.setItem('name', name);
+  const details = document.querySelector('.details').value;
+  localStorage.setItem('details', details);
+});
+
+window.addEventListener('load', (event) => {
+  const name = document.querySelector('.name');
+  name.value = localStorage.getItem('name');
+  const details = document.querySelector('.details');
+  details.value = localStorage.getItem('details');
+});
