02d7badb696289f1621170e12737e62d1732dcf4
Vladislav Klebeko <vlad000624@mail.ru>
2021-03-26 17:02:53 +0300

    Added form submission.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import './main.css';
-import styles from './index.module.css';
-import "./sass/main-second.scss";
+let submit = document.querySelector('#form1');
 
-async function print() {
-  // Here we are using dynamic import
-  const { greet } = await import('./greet');
-  const response = await greet('John Doe');
-  console.log(response);
-}
+submit.addEventListener('submit', () => {
+  let name = document.querySelector('#fname-parent').value;
+  let newName = name.replace(/отД:/g, '');
+  let textArea = document.querySelector('#teaxtarea-parent').value;
+  let newTextArea = textArea.replace(/отД:/g, '');
 
-print();
+  localStorage.setItem('parentName', newName);
+  localStorage.setItem('parentText',newTextArea);
+})
 
+window.addEventListener('load', () => {
 
+  let div = document.createElement('div');
+  let em = document.getElementById("form1");
+  div.classList.add('received__text');
+  div.innerHTML = "<strong>отД</strong>";
+  em.append(div);
+
+  if(localStorage.getItem('childName')){
+    document.querySelector('#fname-parent').value = localStorage.getItem('childName');
+  }
+  if( localStorage.getItem('childText')){
+    document.querySelector('#teaxtarea-parent').value = localStorage.getItem('childText');
+  }
+})
 
 
---
+++ src/lesson1-2.js
@@ -0,0 +1,31 @@
+let submit = document.querySelector('#form2');
+
+submit.addEventListener('submit', () => {
+  let name = document.querySelector('#name-child').value;
+  let newName = name.replace(/отР:/g, '');
+  let textArea = document.querySelector('#teaxtarea-child').value;
+  let newTextArea = textArea.replace(/отР:/g, '');
+
+  localStorage.setItem('childName', newName);
+  localStorage.setItem('childText', newTextArea);
+})
+
+window.addEventListener('load', () => {
+
+  let scale = document.getElementById("main2");
+  scale.classList.add('up__text');
+
+  let div = document.createElement('div');
+  let fromP = document.getElementById("form2");
+  div.classList.add('received__text');
+  div.innerHTML = "<strong>отР</strong>";
+  fromP.append(div);
+
+  if(localStorage.getItem('parentName')){
+    document.querySelector('#name-child').value = localStorage.getItem('parentName');
+  }
+  if(localStorage.getItem('parentText')){
+    document.querySelector('#teaxtarea-child').value = localStorage.getItem('parentText');
+  }
+})
+
