6a2d2c994a165caffa2b6645f79da36e5eb59fb6
Filipichev <d.filipichev@andersenlab.com>
2021-03-26 09:30:28 +0200

    Lesson 5 initial.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import './main.css';
 import styles from './index.module.css';
 import "./sass/main-second.scss";
 
-// const h1 = document.querySelector('h1');
-// h1.classList.add(styles.h1);
-
 async function print() {
   // Here we are using dynamic import
   const { greet } = await import('./greet');
@@ -14,7 +11,61 @@ async function print() {
 
 print();
 
-let res = document.querySelector('.seasons').firstElementChild.innerHTML = 'MOUSE';
+
+function initContactUsFormValidation() {
+  const nameInput = document.getElementById('contact-form__name');
+  if (!nameInput) { return; }
+  const emailInput = document.getElementById('contact-form__email');
+  const phoneInput = document.getElementById('contact-form__phone');
+ 
+  const nameErrorHolder = document.getElementById('contact-form__name--error-message');
+  const emailErrorHolder = document.getElementById('contact-form__email--error-message');
+  const phoneErrorHolder = document.getElementById('contact-form__phone--error-message');
+  const globalFormErrorHolder = document.getElementById('contact-form__button--error-message');
+  const buttonSend = document.getElementById('contact-form__button-send');
+
+  Possible phone numbers: +1234567890 1234567890
+  nameInput.addEventListener('blur', (event) => validateInput(nameErrorHolder, nameInput.value, "personName"));
+  emailInput.addEventListener('blur', (event) => validateEmailOrPhone(phoneErrorHolder, emailErrorHolder, emailInput.value, phoneInput.value));
+  phoneInput.addEventListener('blur', (event) => validateEmailOrPhone(phoneErrorHolder, emailErrorHolder, emailInput.value, phoneInput.value));
+  
+  buttonSend.addEventListener('click', (event) => {
+     event.preventDefault();
+     validateAllBeforeSend(nameInput, emailInput, phoneInput,
+                           nameErrorHolder,nemailErrorHolder, phoneErrorHolder);
+  });
+}
 
 
+function validateAllBeforeSend(name, email, phone, nameErrorHolder, emailErrorHolder, phoneErrorHolder) {
+  while (privacyPolicyErrorHolder.firstChild) {
+    privacyPolicyErrorHolder.firstChild.remove();
+  }
+  if (/^([а-яА-ЯёЁa-zA-Z '-]){2,60}$/g.test(name.value) && 
+    (/^\w+@\w+\.\w{2,3}$/g.test(email.value) || /^\+?\d{5,12}$/g.test(phone.value))) {
+
+    let emailToSend;
+    let phoneToSend;
+
+    if (/^\w+\.?\w+@\w+\.\w{2,3}$/g.test(email.value)) {
+      emailToSend = email.value;
+    }
+
+    if (/^\+?\d{5,12}$/g.test(phone.value)) {
+      phoneToSend = phone.value;
+    }
+
+    let dataToSend = { name: name.value, email: emailToSend, phone: phoneToSend };
+    if (!emailToSend) { delete dataToSend.emailToSend; }
+    if (!phoneToSend) { delete dataToSend.phoneToSend; }
+
+    sendContactUsForm(dataToSend);
+
+    clearForm(name, companyName, email, phone, country, comments, hasAgreedToPrivacyPolicy);
+
+  } else {
+    validateInput(nameErrorHolder, name.value, "personName");
+    validateEmailOrPhone(phoneErrorHolder, emailErrorHolder, email.value, phone.value);
+  }
+}
 
