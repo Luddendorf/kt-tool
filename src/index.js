7547d9953b819e7b12e2082b0d342a8aa21d6638
vladlol001 <yakavitskiy001@mail.ru>
2021-03-29 12:54:20 +0300

    add homework

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import './main.css';
 import styles from './index.module.css';
 import "./sass/main-second.scss";
 
-async function print() {
-  // Here we are using dynamic import
-  const { greet } = await import('./greet');
-  const response = await greet('John Doe');
-  console.log(response);
-}
 
-print();
+const burger = document.querySelector('.header__burger');
+const modalBurger = document.querySelector('.menu__burger');
 
-
-
-
+burger.addEventListener('click', () => {
+  modalBurger.classList.toggle('hide');
+})
+window.addEventListener('load', () => {
+  modalBurger.classList.add('hide');
+})
+window.addEventListener('resize', (e) => {
+  if(document.body.offsetWidth > 567){
+    modalBurger.classList.add('hide');
+  }
+})
