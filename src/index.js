30dad7026ea359e60110f07dc00fdecf58113e02
Filipichev <d.filipichev@andersenlab.com>
2021-04-02 21:10:03 +0300

    Lesson 4 initial commit.

--- src/index.js
+++ src/index.js
@@ -11,38 +11,47 @@ async function print() {
 print();
 
 
-const unsorted = [
-	{
-		id: 'c744e61d-18ab-4df1-9bf2-281bd7fcf02e',
-		profile: 'ordinary',
-		type: 'company',
-		amount: 59237
-	},
-	{
-		id: '8d7a1980-c9ec-4103-aac6-0a6a4d9890cb',
-		profile: 'privileged',
-		type: 'company',
-		amount: 70832
-	},
-	{
-		id: '5cf215d3-c931-4939-9c86-f906319c13ea',
-		profile: 'foreign',
-		type: 'subsidiary',
-		amount: 17341
-	},
-	{
-		id: '5b0d93c5-99f0-41ff-bdfb-9ec754745d68',
-		profile: 'foreign',
-		type: 'store',
-		amount: 60391
-	},
-	{
-		id: '4b5e78df-7872-4c10-b25c-97284dbb2177',
-		profile: 'ordinary',
-		type: 'store',
-		amount: 45972
-	}
-];
+// 2-e задание 
+var snickersInput = document.getElementById('snickers');
+
+function sparky() {
+	console.log(this.value);
+}
+
+sparky(); // должно появится value элемента snickers
+
+const person = {
+	nick: 'Bob',
+	secondName: 'Johnson'
+};
+
+function greeter(surname, name) {
+	console.log(this.value + ', ' + surname + ' ' + name);
+}
+
+greeter(person.secondName, person.nick);
+// должно появится value элемента snickers + Bob Johnson
 
 
+// 3-е задание
+const path = {
+  mile: 0,
+  forward() {
+    this.mile++;
+  },
+  backward() {
+    this.mile--;
+  },
+  stayInPlace(){
+  	this.mile = this.mile;
+  },
+  goToStart() {
+  	this.mile = 0;
+  }
+  showMile: function() { // показывает текущую милю
+    console.log(this.mile);
+  }
+};
 
+// Возращает 1 
+path.stayInPlace().goToStart().forward().backward().forward().showMile();
