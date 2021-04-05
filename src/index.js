ce2eed5a3712e2c2eeb40abb50c239e804f63ef5
apollon313131 <apollo-bel@mail.ru>
2021-04-05 14:14:46 +0300

    add task 3

--- src/index.js
+++ src/index.js
@@ -3,54 +3,60 @@ import styles from './index.module.css';
 import "./sass/main-second.scss";
 
 async function print() {
-  // Here we are using dynamic import
-  const { greet } = await import('./greet');
-  const response = await greet('John Doe');
+    // Here we are using dynamic import
+    const { greet } = await
+    import ('./greet');
+    const response = await greet('John Doe');
 }
 
 print();
 
 
-// 2-e задание 
-var snickersInput = document.getElementById('snickers');
+// // 2-e задание 
+// var snickersInput = document.getElementById('snickers');
 
-function sparky() {
-	console.log(this.value);
-}
+// function sparky() {
+//     console.log(this.value);
+// }
 
-sparky(); // должно появится value элемента snickers
+// sparky(); // должно появится value элемента snickers
 
-const person = {
-	nick: 'Bob',
-	secondName: 'Johnson'
-};
+// const person = {
+//     nick: 'Bob',
+//     secondName: 'Johnson'
+// };
 
-function greeter(surname, name) {
-	console.log(this.value + ', ' + surname + ' ' + name);
-}
+// function greeter(surname, name) {
+//     console.log(this.value + ', ' + surname + ' ' + name);
+// }
 
-greeter(person.secondName, person.nick);
-// должно появится value элемента snickers + Bob Johnson
+// greeter(person.secondName, person.nick);
+// // должно появится value элемента snickers + Bob Johnson
 
 
 // 3-е задание
 const path = {
-  mile: 0,
-  forward() {
-    this.mile++;
-  },
-  backward() {
-    this.mile--;
-  },
-  stayInPlace(){
-  	this.mile = this.mile;
-  },
-  goToStart() {
-  	this.mile = 0;
-  }
-  showMile: function() { // показывает текущую милю
-    console.log(this.mile);
-  }
+    mile: 0,
+    forward() {
+        console.log(this.mile);
+        this.mile++;
+        return this;
+    },
+    backward() {
+        this.mile--;
+        return this;
+    },
+    stayInPlace() {
+        this.mile = this.mile;
+        return this;
+    },
+    goToStart() {
+        this.mile = 0;
+        return this;
+    },
+    showMile: function() { // показывает текущую милю
+        console.log(this.mile);
+    }
 };
 
 // Возращает 1 
