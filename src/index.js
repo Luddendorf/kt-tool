1922930558d0e4c0cc4ccbdf93a1857b46ec30fe
apollon313131 <apollo-bel@mail.ru>
2021-04-05 13:10:28 +0300

    edit task 1 - add ui and if

--- src/index.js
+++ src/index.js
@@ -13,6 +13,18 @@ const areaOut = document.querySelector('#commentOut');
 const textInput = document.querySelector('#textInput');
 const textOutput = document.querySelector('#textOutput');
 
+
+
+btnRevers.addEventListener('click', function() {
+    const textInp = textInput.value;
+    // const textOutp = textOutput.value;
+    // let arr2 = [1, 2, 3, 4, 5, 6]
+    // let newArr = [];
+    // let leng = textInp.length;
+    solution(textInp, textOutput);
+});
+
+
 sliceButton.addEventListener('click', function() {
     const com = areaComment.value;
     const kol = kSimvol.value;
@@ -24,23 +36,35 @@ sliceButton.addEventListener('click', function() {
 // const k = 14
 
 
-let arr = [1, 2, 3, 4, 5, 6]
-let newArr = []
-let leng = arr.length
+// let arr2 = [1, 2, 3, 4, 5, 6]
+// let newArr = []
+// let leng = arr2.length
 
-function solution(arr) {
-    while (newArr.length !== leng) {
-        newArr.push(arr[0])
-        arr.shift();
-        arr.reverse()
-        console.log('1', arr);
+function solution(textInp, textOutput) {
+    if (textInp <= 100000000 && textInp > 0) {
+        let textM = textInp.split("");
+        console.log('Massiv', textM);
+        let newArr = [];
+        let leng = textM.length;
+        while (newArr.length !== leng) {
+            newArr.push(textM[0]);
+            textM.shift();
+            textM.reverse();
+            console.log('1', textM);
+        }
+        console.log('textM', textM);
+        console.log('newArr', newArr);
+        // const newArrayOut = newArr.join()
+        let newArrayOut = newArr.join("")
+        console.log('newArrayOut', typeof newArrayOut);
+        textOutput.value = newArrayOut;
+    } else {
+        console.log("This number doesn't fit")
+        textOutput.value = "This number is not suitable, use from 0 to 100 000 000"
     }
-
-    console.log('arr', arr);
-    console.log('newArr', newArr);
 }
 
-solution(arr)
+// solution(arr2)
 
 
 function funcSlice(comment, k) {
