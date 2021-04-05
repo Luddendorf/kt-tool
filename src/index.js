4849edbe574597a0f23c35b3bb6651f2f20eed41
apollon313131 <apollo-bel@mail.ru>
2021-04-05 14:22:51 +0300

    add task 2

--- src/index.js
+++ src/index.js
@@ -12,26 +12,26 @@ async function print() {
 print();
 
 
-// // 2-e задание 
-// var snickersInput = document.getElementById('snickers');
+// 2-e задание 
+var snickersInput = document.getElementById('snickers');
 
-// function sparky() {
-//     console.log(this.value);
-// }
+function sparky() {
+    console.log(this.value);
+}
 
-// sparky(); // должно появится value элемента snickers
+sparky.apply(snickersInput); // должно появится value элемента snickers
 
-// const person = {
-//     nick: 'Bob',
-//     secondName: 'Johnson'
-// };
+const person = {
+    nick: 'Bob',
+    secondName: 'Johnson'
+};
 
-// function greeter(surname, name) {
-//     console.log(this.value + ', ' + surname + ' ' + name);
-// }
+function greeter(surname, name) {
+    console.log(this.value + ', ' + surname + ' ' + name);
+}
 
-// greeter(person.secondName, person.nick);
-// // должно появится value элемента snickers + Bob Johnson
+let res = greeter.bind(snickersInput, person.nick, person.secondName)();
+// должно появится value элемента snickers + Bob Johnson
 
 
 // 3-е задание
