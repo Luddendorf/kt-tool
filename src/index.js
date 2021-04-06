fd3087a70ea7fe6739d9be88e22269793d8e0ee7
apollon313131 <apollo-bel@mail.ru>
2021-04-06 13:27:21 +0300

    add task 6 Date()

--- src/index.js
+++ src/index.js
@@ -12,12 +12,70 @@ async function print() {
 print();
 
 
+
+// 1-e задание 
+//f1 = a 
+// f2 = b
+// obj1 = c
+// obj2 = d
+
+function main(f1, f2, obj1, obj2) {
+    let str = 'данные основной функции + '
+    let resA = f1(f2, obj1, obj2)
+
+    let resMain = str + resA
+
+    console.log('resMain', resMain);
+    return resMain
+
+}
+
+
+function f1(b, c, d) {
+
+    let resB = (d)
+    console.log('resB', resB);
+
+    c = {
+        id: 'измененный C'
+    }
+    console.log('c', c);
+
+    let resA = `${c.id} + ${resB.id}`
+    console.log('resA = ', resA);
+
+    return resA
+}
+
+function f2(d) {
+    let resB = d = {
+        id: 'измененный D'
+    }
+    return resB
+}
+
+const obj1 = {
+    id: 'null'
+}
+
+const obj2 = {
+    id: 'null'
+}
+
+
+let result = main(f1, f2, obj1, obj2)
+console.log('result', result);
+
+
+
+
+
 // 2-e задание 
 var snickersInput = document.getElementById('snickers');
 
 function sparky() {
     console.log(this.value);
-}
+};
 
 sparky.apply(snickersInput); // должно появится value элемента snickers
 
@@ -28,7 +86,7 @@ const person = {
 
 function greeter(surname, name) {
     console.log(this.value + ', ' + surname + ' ' + name);
-}
+};
 
 let res = greeter.bind(snickersInput, person.nick, person.secondName)();
 // должно появится value элемента snickers + Bob Johnson
@@ -72,7 +130,7 @@ let
     mult = 'mult',
     div = 'div';
 
-function f1(arr, action) {
+function fOne(arr, action) {
     switch (action) {
         case 'sum':
             console.log(arr[0] + arr[1]);
@@ -95,9 +153,43 @@ function f1(arr, action) {
     }
 }
 
-let summ = f1.bind(null, arr, sum)
-let subb = f1.bind(null, arr, sub)
-let multt = f1.bind(null, arr, mult)
-let divv = f1.bind(null, arr, div)
+let summ = fOne.bind(null, arr, sum);
+let subb = fOne.bind(null, arr, sub);
+let multt = fOne.bind(null, arr, mult);
+let divv = fOne.bind(null, arr, div);
 
 divv()
+
+
+
+//6 задание
+const daysT = document.getElementById('days');
+const hoursT = document.getElementById('hours');
+const minsT = document.getElementById('mins');
+const secondsT = document.getElementById('seconds');
+const newYear = '1 Jan 2022';
+
+function countdown() {
+    const newYearsDate = new Date(newYear);
+    const currentDate = new Date();
+    const secondsCount = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(secondsCount / 3600 / 24);
+    const hours = Math.floor(secondsCount / 3600) % 24;
+    const minutes = Math.floor(secondsCount / 60) % 60;
+    const seconds = Math.floor(secondsCount) % 60
+        // console.log(days, hours, minutes, seconds);
+
+
+
+    daysT.innerHTML = days;
+    hoursT.innerHTML = hours;
+    minsT.innerHTML = minutes;
+    secondsT.innerHTML = seconds;
+
+}
+
+
+
+countdown();
+
+setInterval(countdown, 1000)
