eefb19308c98b5e7d7305381ac53b2e32910461a
unknown <staslukyanenko1995@gmail.com>
2021-04-08 05:17:43 +0300

    fix: menu

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+
+'use strict';
+
+const accordion = () => {
+    const leftMenu = document.querySelector('.left-menu');
+    const leftMenuItems = document.querySelectorAll('.left-menu-item');
+
+    const open = (button, dropDown) => {
+        closeAllDrops();
+        dropDown.style.height = `${dropDown.scrollHeight}px`;
+        button.classList.add('active');
+        dropDown.classList.add('active');
+    };
+
+    const close = (button, dropDown) => {
+        button.classList.remove('active');
+        dropDown.classList.remove('active');
+        dropDown.style.height = '';
+    };
+
+    const closeAllDrops = (button, dropDown) => {
+        leftMenuItems.forEach((elem) => {
+            if (elem.children[0] !== button && elem.children[1] !== dropDown) {
+                close(elem.children[0], elem.children[1]);
+            }
+        })
+    };
+
+    leftMenu.addEventListener('click', (event) => {
+        console.log(event.target);
+        const target = event.target;
+        if (target.classList.contains('left-menu-item-button')) {
+            const parent = target.closest('.left-menu-item');
+            const description = parent.querySelector('.left-menu-description');
+
+            description.classList.contains('active') ?
+                close(target, description) :
+                    open(target, description);
+        }
+    })
+
+    document.body.addEventListener('click', (event) => {
+        const target = event.target;
+        if (!target.closest('.left-menu')) {
+            closeAllDrops();
+        }
+    })
+};
+
+
+accordion();
