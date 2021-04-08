d121d617a66a5d75d5c77b02ab988fa6b4f4caea
SaltyBr <SaltyBrain@yandex.ru>
2021-04-08 18:56:08 +0300

    добавил аккордеон на страницу

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import './main.css';
 import styles from './index.module.css';
 import "./sass/main-second.scss";
+import accordeon from './modules/accordeon';
 
-async function print() {
-  // Here we are using dynamic import
-  const { greet } = await import('./greet');
-  const response = await greet('John Doe');
-  console.log(response);
-}
-
-print();
+accordeon();
 
 const ships = [
   { 
---
+++ src/modules/accordeon.js
@@ -0,0 +1,33 @@
+const accordeon = () => {
+  const accordButtons = document.querySelectorAll('.aside__button');
+  const asideLists = document.querySelectorAll('.aside__list');
+
+  accordButtons.forEach((button, index)=>{
+    button.addEventListener('click', (event)=>{
+      event.preventDefault();
+      const content = button.nextElementSibling;
+      
+      if(content.style.maxHeight){
+        content.style.maxHeight = null;
+        button.classList.remove('is_open');
+      } else{
+        content.style.maxHeight = content.scrollHeight + 'px';
+        button.classList.add('is_open');
+      }
+
+      asideLists.forEach((listItem)=>{
+        if(listItem !== content){
+          listItem.style.maxHeight = null;
+        }
+      })
+
+      accordButtons.forEach((item)=>{
+        if(item !== button){
+          item.classList.remove('is_open');
+        }
+      })
+    })
+  })
+}
+
+export default accordeon;
