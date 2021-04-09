27e8da80e89beb0d385b430889940a1dd881729e
SaltyBr <SaltyBrain@yandex.ru>
2021-04-09 14:45:32 +0300

    добавил фильтрацию (пока криво)

--- src/index.js
+++ src/index.js
@@ -283,14 +283,119 @@ ships.forEach((card)=>{
   </div>`);
 });
 
+// фильтр по минимальной цене
 lowPrice.addEventListener('blur', ()=>{
+  cardsWrapper.innerHTML = null;
   ships.filter((card)=>{
-    if(lowPrice.value >= card.price){
-      console.log(card.price);
+    if(card.price > lowPrice.value){
+      return   cardsWrapper.insertAdjacentHTML('beforeend', `
+      <div class="main__card">
+        <a href="#close" title="Закрыть" class="close">X</a>
+      <div class="main__card-img">
+        <img src="${card.url}" alt="ship">
+      </div>
+        <ul class="main__list">
+          <li class="main__list-item"><span>${card.className}</span>: ${card.shipName}</li>
+          <li class="main__list-item"><span>Класс</span>: ${card.class}</li>
+          <li class="main__list-item"><span>Пушки</span>: ${card.weapons}</li>
+          <li class="main__list-item"><span>Корпус</span>: ${card.shipHull}</li>
+          <li class="main__list-item"><span>Трюм</span>: ${card.hold}</li>
+          <li class="main__list-item"><span>Скорость</span>: ${card.speed}</li>
+          <li class="main__list-item"><span>Манёвренность</span>: ${card.maneuverability}</li>
+          <li class="main__list-item"><span>Команда</span>: ${card.team}</li>
+          <li class="main__list-item"><span>Цена</span>: ${card.price}</li>
+          <li class="main__list-item"><span>Описание</span>: ${card.desc}</li>
+        </ul>
+      </div>`);
     }
-  })
-})
+  });
+});
 
+// фильтр по максимальной цене
+maxPrice.addEventListener('blur', ()=>{
+  cardsWrapper.innerHTML = null;
+  ships.filter((card)=>{
+    if(card.price < maxPrice.value){
+      return   cardsWrapper.insertAdjacentHTML('beforeend', `
+      <div class="main__card">
+        <a href="#close" title="Закрыть" class="close">X</a>
+      <div class="main__card-img">
+        <img src="${card.url}" alt="ship">
+      </div>
+        <ul class="main__list">
+          <li class="main__list-item"><span>${card.className}</span>: ${card.shipName}</li>
+          <li class="main__list-item"><span>Класс</span>: ${card.class}</li>
+          <li class="main__list-item"><span>Пушки</span>: ${card.weapons}</li>
+          <li class="main__list-item"><span>Корпус</span>: ${card.shipHull}</li>
+          <li class="main__list-item"><span>Трюм</span>: ${card.hold}</li>
+          <li class="main__list-item"><span>Скорость</span>: ${card.speed}</li>
+          <li class="main__list-item"><span>Манёвренность</span>: ${card.maneuverability}</li>
+          <li class="main__list-item"><span>Команда</span>: ${card.team}</li>
+          <li class="main__list-item"><span>Цена</span>: ${card.price}</li>
+          <li class="main__list-item"><span>Описание</span>: ${card.desc}</li>
+        </ul>
+      </div>`);
+    }
+  });
+});
+
+// фильтрация по мин прочности
+lowDur.addEventListener('blur', ()=>{
+  cardsWrapper.innerHTML = null;
+  ships.filter((card)=>{
+    if(card.shipHull > lowDur.value){
+      return   cardsWrapper.insertAdjacentHTML('beforeend', `
+      <div class="main__card">
+        <a href="#close" title="Закрыть" class="close">X</a>
+      <div class="main__card-img">
+        <img src="${card.url}" alt="ship">
+      </div>
+        <ul class="main__list">
+          <li class="main__list-item"><span>${card.className}</span>: ${card.shipName}</li>
+          <li class="main__list-item"><span>Класс</span>: ${card.class}</li>
+          <li class="main__list-item"><span>Пушки</span>: ${card.weapons}</li>
+          <li class="main__list-item"><span>Корпус</span>: ${card.shipHull}</li>
+          <li class="main__list-item"><span>Трюм</span>: ${card.hold}</li>
+          <li class="main__list-item"><span>Скорость</span>: ${card.speed}</li>
+          <li class="main__list-item"><span>Манёвренность</span>: ${card.maneuverability}</li>
+          <li class="main__list-item"><span>Команда</span>: ${card.team}</li>
+          <li class="main__list-item"><span>Цена</span>: ${card.price}</li>
+          <li class="main__list-item"><span>Описание</span>: ${card.desc}</li>
+        </ul>
+      </div>`);
+    }
+  });
+});
+
+//фильтрация по макс прочности
+maxDur.addEventListener('blur', ()=>{
+  cardsWrapper.innerHTML = null;
+  ships.filter((card)=>{
+    if(card.shipHull < maxDur.value){
+      return   cardsWrapper.insertAdjacentHTML('beforeend', `
+      <div class="main__card">
+        <a href="#close" title="Закрыть" class="close">X</a>
+      <div class="main__card-img">
+        <img src="${card.url}" alt="ship">
+      </div>
+        <ul class="main__list">
+          <li class="main__list-item"><span>${card.className}</span>: ${card.shipName}</li>
+          <li class="main__list-item"><span>Класс</span>: ${card.class}</li>
+          <li class="main__list-item"><span>Пушки</span>: ${card.weapons}</li>
+          <li class="main__list-item"><span>Корпус</span>: ${card.shipHull}</li>
+          <li class="main__list-item"><span>Трюм</span>: ${card.hold}</li>
+          <li class="main__list-item"><span>Скорость</span>: ${card.speed}</li>
+          <li class="main__list-item"><span>Манёвренность</span>: ${card.maneuverability}</li>
+          <li class="main__list-item"><span>Команда</span>: ${card.team}</li>
+          <li class="main__list-item"><span>Цена</span>: ${card.price}</li>
+          <li class="main__list-item"><span>Описание</span>: ${card.desc}</li>
+        </ul>
+      </div>`);
+    }
+  });
+});
+
+// открытие карточек в модальном окне
 cardsWrapper.addEventListener('click', (event)=>{
   let target = event.target;
   target = target.closest('.main__card');
