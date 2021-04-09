d0e5de353553bd64bc3c5ea3cab1263e21680da5
Ervin Abliamitov <rwin321321@gmail.com>
2021-04-09 15:52:54 +0300

    ervin-abliamitov-lesson5 (not finished)

--- src/index.js
+++ src/index.js
@@ -1,49 +1,78 @@
-import './main.css';
-import styles from './index.module.css';
-import "./sass/main-second.scss";
-
-async function print() {
-  // Here we are using dynamic import
-  const { greet } = await import('./greet');
-  const response = await greet('John Doe');
-  console.log(response);
-}
-
-print();
-
+import "./sass/main.scss";
+import "./components/main/mainContent";
 
 function initContactUsFormValidation() {
-  const nameInput = document.getElementById('contact-form__name');
-  if (!nameInput) { return; }
-  const emailInput = document.getElementById('contact-form__email');
-  const phoneInput = document.getElementById('contact-form__phone');
- 
-  const nameErrorHolder = document.getElementById('contact-form__name--error-message');
-  const emailErrorHolder = document.getElementById('contact-form__email--error-message');
-  const phoneErrorHolder = document.getElementById('contact-form__phone--error-message');
-  const globalFormErrorHolder = document.getElementById('contact-form__button--error-message');
-  const buttonSend = document.getElementById('contact-form__button-send');
+  const nameInput = document.getElementById("contact-form__name");
+  if (!nameInput) {
+    return;
+  }
+  const emailInput = document.getElementById("contact-form__email");
+  const phoneInput = document.getElementById("contact-form__phone");
 
-  Possible phone numbers: +1234567890 1234567890
-  nameInput.addEventListener('blur', (event) => validateInput(nameErrorHolder, nameInput.value, "personName"));
-  emailInput.addEventListener('blur', (event) => validateEmailOrPhone(phoneErrorHolder, emailErrorHolder, emailInput.value, phoneInput.value));
-  phoneInput.addEventListener('blur', (event) => validateEmailOrPhone(phoneErrorHolder, emailErrorHolder, emailInput.value, phoneInput.value));
-  
-  buttonSend.addEventListener('click', (event) => {
-     event.preventDefault();
-     validateAllBeforeSend(nameInput, emailInput, phoneInput,
-                           nameErrorHolder,nemailErrorHolder, phoneErrorHolder);
+  const nameErrorHolder = document.getElementById(
+    "contact-form__name--error-message"
+  );
+  const emailErrorHolder = document.getElementById(
+    "contact-form__email--error-message"
+  );
+  const phoneErrorHolder = document.getElementById(
+    "contact-form__phone--error-message"
+  );
+  const globalFormErrorHolder = document.getElementById(
+    "contact-form__button--error-message"
+  );
+  const buttonSend = document.getElementById("contact-form__button-send");
+
+  // Possible phone numbers: +1234567890 1234567890
+  nameInput.addEventListener("blur", (event) =>
+    validateInput(nameErrorHolder, nameInput.value, "personName")
+  );
+  emailInput.addEventListener("blur", (event) =>
+    validateEmailOrPhone(
+      phoneErrorHolder,
+      emailErrorHolder,
+      emailInput.value,
+      phoneInput.value
+    )
+  );
+  phoneInput.addEventListener("blur", (event) =>
+    validateEmailOrPhone(
+      phoneErrorHolder,
+      emailErrorHolder,
+      emailInput.value,
+      phoneInput.value
+    )
+  );
+
+  buttonSend.addEventListener("click", (event) => {
+    event.preventDefault();
+    validateAllBeforeSend(
+      nameInput,
+      emailInput,
+      phoneInput,
+      nameErrorHolder,
+      emailErrorHolder,
+      phoneErrorHolder
+    );
   });
 }
 
-
-function validateAllBeforeSend(name, email, phone, nameErrorHolder, emailErrorHolder, phoneErrorHolder) {
+function validateAllBeforeSend(
+  name,
+  email,
+  phone,
+  nameErrorHolder,
+  emailErrorHolder,
+  phoneErrorHolder
+) {
   while (privacyPolicyErrorHolder.firstChild) {
     privacyPolicyErrorHolder.firstChild.remove();
   }
-  if (/^([а-яА-ЯёЁa-zA-Z '-]){2,60}$/g.test(name.value) && 
-    (/^\w+@\w+\.\w{2,3}$/g.test(email.value) || /^\+?\d{5,12}$/g.test(phone.value))) {
-
+  if (
+    /^([а-яА-ЯёЁa-zA-Z '-]){2,60}$/g.test(name.value) &&
+    (/^\w+@\w+\.\w{2,3}$/g.test(email.value) ||
+      /^\+?\d{5,12}$/g.test(phone.value))
+  ) {
     let emailToSend;
     let phoneToSend;
 
@@ -55,17 +84,36 @@ function validateAllBeforeSend(name, email, phone, nameErrorHolder, emailErrorHo
       phoneToSend = phone.value;
     }
 
-    let dataToSend = { name: name.value, email: emailToSend, phone: phoneToSend };
-    if (!emailToSend) { delete dataToSend.emailToSend; }
-    if (!phoneToSend) { delete dataToSend.phoneToSend; }
+    let dataToSend = {
+      name: name.value,
+      email: emailToSend,
+      phone: phoneToSend,
+    };
+    if (!emailToSend) {
+      delete dataToSend.emailToSend;
+    }
+    if (!phoneToSend) {
+      delete dataToSend.phoneToSend;
+    }
 
     sendContactUsForm(dataToSend);
 
-    clearForm(name, companyName, email, phone, country, comments, hasAgreedToPrivacyPolicy);
-
+    clearForm(
+      name,
+      companyName,
+      email,
+      phone,
+      country,
+      comments,
+      hasAgreedToPrivacyPolicy
+    );
   } else {
     validateInput(nameErrorHolder, name.value, "personName");
-    validateEmailOrPhone(phoneErrorHolder, emailErrorHolder, email.value, phone.value);
+    validateEmailOrPhone(
+      phoneErrorHolder,
+      emailErrorHolder,
+      email.value,
+      phone.value
+    );
   }
 }
-
