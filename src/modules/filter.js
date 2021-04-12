87aafdc89c7f6ec9ff5359cd670c3dd0110b9679
SaltyBr <SaltyBrain@yandex.ru>
2021-04-12 15:33:51 +0300

    сделал работающий фильтр

--- src/modules/filter.js
+++ src/modules/filter.js
@@ -2,58 +2,46 @@ import { ships } from './data';
 import renderData from './renderData';
 
 const filter = () => {
-  const cardsWrapper = document.querySelector(".main__cards-wrapper");
-  const lowPrice = document.getElementById("low-price");
-  const maxPrice = document.getElementById("max-price");
 
-  renderData(ships, cardsWrapper);
+  renderData(ships, document.querySelector(".main__cards-wrapper"));
 
   // функция фильтрации
-  const filterMinMAx = (arr, el, operation)=>{
-    return arr.filter((item)=>{
-      if(operation === 'min'){
-        if(item.price > el.value){
-          return item;
-        }
-      } else if(operation === 'max'){
-        if(item.price < el.value){
-          return item;
-        } else if(el.value == ''){
-          return item;
-        }
-      }
-    })
-  }
+  const minPrice = document.getElementById('min-price');
+  const maxPrice = document.getElementById('max-price');
+  const minDur = document.getElementById('min-dur');
+  const maxDur = document.getElementById('max-dur');
 
-  // фильтр по минимальной цене
-  lowPrice.addEventListener("blur", () => {
-    if(lowPrice.value.trim() !== ''){
-      let result = filterMinMAx(ships, lowPrice, 'min');
-      cardsWrapper.innerHTML = '';
-      renderData(result, cardsWrapper);
+    const filterHandler = (arr)=>{
   
-      maxPrice.addEventListener("blur", () => {
-        filterMinMAx(result, maxPrice, 'max');
-        cardsWrapper.innerHTML = '';
-        renderData(filterMinMAx(result, maxPrice, 'max'), cardsWrapper)
-      });
-    }
-  });
-
-  // фильтр по максимальной цене
-  maxPrice.addEventListener("blur", () => {
-    if(lowPrice.value.trim() !== ''){
-      let result = filterMinMAx(ships, maxPrice, 'max');
-      cardsWrapper.innerHTML = '';
-      renderData(result, cardsWrapper);
-
-      lowPrice.addEventListener("blur", () => {
-        filterMinMAx(result, lowPrice, 'min');
-        cardsWrapper.innerHTML = '';
-        renderData(filterMinMAx(result, lowPrice, 'min'), cardsWrapper)
-      });
-    }
-  });
+    document.querySelector('.main__cards-wrapper').innerHTML = '';
+  
+    const filteredShips = arr.filter(ship => {
+  
+      if(ship.price < minPrice.value && minPrice.value !== ""){
+        return false
+      }
+      if(ship.price > maxPrice.value && maxPrice.value !== ""){
+        return false
+      }
+      if(ship.shipHull < minDur.value && minDur.value !== ""){
+        return false
+      }
+      if(ship.shipHull > maxDur.value && maxDur.value !== ""){
+        return false
+      }
+  
+      return true
+    
+    });
+  
+    renderData(filteredShips, document.querySelector('.main__cards-wrapper'));
+  }
+  
+  
+  minPrice.addEventListener('change', filterHandler.bind(null, ships));
+  maxPrice.addEventListener('change', filterHandler.bind(null, ships));
+  minDur.addEventListener('change', filterHandler.bind(null, ships));
+  maxDur.addEventListener('change', filterHandler.bind(null, ships));
 };
 
 export default filter;
