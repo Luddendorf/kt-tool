a12fbc491818c358e3e19d2f38716787ee3c2e7b
SaltyBr <SaltyBrain@yandex.ru>
2021-04-12 22:42:27 +0300

    подключил api к сайту

--- src/modules/fetchTemperature.js
+++ src/modules/fetchTemperature.js
@@ -1,6 +1,15 @@
 
 const fetchTemperature = ()=>{
-  
+  const apiUrl = 'https://api.tomorrow.io/v4/timelines?location=21.607646,-78.922639&fields=temperature&timesteps=1h&units=metric&apikey=bCG4ZtFtqusc9DUeCuvgUsawbMk5H5P3';
+  async function print() {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const result = data.data.timelines[0].intervals;
+
+    console.log(result);
+  }
+
+  print();
 }
 
 export default fetchTemperature;
