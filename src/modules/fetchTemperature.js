b6bc9220456e2ce83a050b591989f4a4bb98ef3b
SaltyBr <SaltyBrain@yandex.ru>
2021-04-13 14:44:44 +0300

    добавил загрузку погоды со стороннего api

--- src/modules/fetchTemperature.js
+++ src/modules/fetchTemperature.js
@@ -13,23 +13,29 @@ const fetchTemperature = ()=>{
   const dateCells = document.querySelectorAll('.date-cell');
   const tempCells = document.querySelectorAll('.temp-cell');
 
-    const apiUrl = 'https://api.tomorrow.io/v4/timelines?location=21.607646,-78.922639&fields=temperature&timesteps=1h&units=metric&apikey=bCG4ZtFtqusc9DUeCuvgUsawbMk5H5P3';
+    const apiUrl = 'https://api.tomorrow.io/v4/timelines?location=21.607646,-78.922639&fields=temperature&timesteps=1h&units=metric&apikey=C6U2pWsF7y2ih57cDvKUwdjHC2wM8hWf';
     async function getData() {
       const response = await fetch(apiUrl);
       const data = await response.json();
-      const result = Object.assign({}, data.data.timelines[0].intervals);
 
-    //   console.log(result);
+      const result = data.data.timelines[0].intervals;
 
-    //   let date = new Date(result[0].startTime);
+      const regDate = /(\d\d-\d\d).(12:\d\d)/i;
 
-    //   console.log(date.getUTCDate());
+      result.forEach((timeStamp)=>{
+        if(regDate.test(timeStamp.startTime)){
+          const date = timeStamp.startTime.match(regDate);
+          const temp = timeStamp.values.temperature;
+          dateArr.push(date[1].split('-').reverse().join('/'));
+          tempArr.push(`+${Math.ceil(temp)}°C`);
+        }
+      })
+
+    fillCells(dateCells, dateArr);
+    fillCells(tempCells, tempArr);
   }
 
-  //getData();
-
-  fillCells(dateCells, dateArr);
-  fillCells(tempCells, tempArr);
+  getData();
 
 }
 export default fetchTemperature;
