87392e3327cf7fb0a2cafdf29ad0b4e39a2a74ea
SaltyBr <SaltyBrain@yandex.ru>
2021-04-13 14:08:49 +0300

    Update fetchTemperature.js

--- src/modules/fetchTemperature.js
+++ src/modules/fetchTemperature.js
@@ -1,19 +1,19 @@
 
 const fetchTemperature = ()=>{
-  //   const apiUrl = 'https://api.tomorrow.io/v4/timelines?location=21.607646,-78.922639&fields=temperature&timesteps=1h&units=metric&apikey=bCG4ZtFtqusc9DUeCuvgUsawbMk5H5P3';
-  //   async function getData() {
-  //     const response = await fetch(apiUrl);
-  //     const data = await response.json();
-  //     const result = data.data.timelines[0].intervals;
+    const apiUrl = 'https://api.tomorrow.io/v4/timelines?location=21.607646,-78.922639&fields=temperature&timesteps=1h&units=metric&apikey=bCG4ZtFtqusc9DUeCuvgUsawbMk5H5P3';
+    async function getData() {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      const result = Object.assign({}, data);
 
-  //     console.log(result[3].startTime);
+      console.log(result.data.timelines[0].intervals[0].startTime);
 
-  //     let date = new Date(result[3].startTime);
+      let date = new Date(result.data.timelines[0].intervals[1].startTime);
 
-  //     console.log(date);
-  // }
+      console.log(date.getUTCDate());
+  }
 
-  // getData();
+  getData();
 
   const table = document.getElementById('weather-table-id');
 
