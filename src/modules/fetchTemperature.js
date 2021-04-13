4fb13f0c77de342e3d5c9cd61b6f74bce7cb75bb
SaltyBr <SaltyBrain@yandex.ru>
2021-04-13 14:23:05 +0300

    Update fetchTemperature.js

--- src/modules/fetchTemperature.js
+++ src/modules/fetchTemperature.js
@@ -1,26 +1,35 @@
 
 const fetchTemperature = ()=>{
+
+  const fillCells = (_elements, arr)=>{
+    _elements.forEach((cell, index)=>{
+      cell.textContent = arr[index];
+    })
+  };
+
+  const dateArr = [];
+  const tempArr = [];
+  
+  const dateCells = document.querySelectorAll('.date-cell');
+  const tempCells = document.querySelectorAll('.temp-cell');
+
     const apiUrl = 'https://api.tomorrow.io/v4/timelines?location=21.607646,-78.922639&fields=temperature&timesteps=1h&units=metric&apikey=bCG4ZtFtqusc9DUeCuvgUsawbMk5H5P3';
     async function getData() {
       const response = await fetch(apiUrl);
       const data = await response.json();
-      const result = Object.assign({}, data);
+      const result = Object.assign({}, data.data.timelines[0].intervals);
 
-      console.log(result.data.timelines[0].intervals[0].startTime);
+    //   console.log(result);
 
-      let date = new Date(result.data.timelines[0].intervals[1].startTime);
+    //   let date = new Date(result[0].startTime);
 
-      console.log(date.getUTCDate());
+    //   console.log(date.getUTCDate());
   }
 
-  getData();
+  //getData();
 
-  const table = document.getElementById('weather-table-id');
-
-  const dateCells = document.querySelectorAll('.date-cell');
-  const tempCells = document.querySelectorAll('.temp-cell');
-
-  
+  fillCells(dateCells, dateArr);
+  fillCells(tempCells, tempArr);
 
 }
 export default fetchTemperature;
