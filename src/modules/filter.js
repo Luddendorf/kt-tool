409c310fe27caf5479c783a24f4ceea32e47521b
Bulat <bulat100@yandex.ru>
2021-04-15 02:49:34 +0300

    Full homework 2.0

--- src/modules/filter.js
+++ src/modules/filter.js
@@ -3,7 +3,22 @@ import shipCard from "./shipCard";
 
 export default () => {
   let tab1 = document.getElementById('home');
-  const inputMin = document.getElementById("min-price");
-  const inputMax = document.getElementById("max-price");
-  return shipCard(ships, tab1);
+  const minPrice = document.getElementById("min-price");
+  const maxPrice = document.getElementById("max-price");
+  const inputsFilter = document.querySelectorAll('input[type="number"]');
+  
+  inputsFilter.forEach((input) => {
+    input.addEventListener('input', () => {
+      if (input.value === "") {
+        return shipCard(ships,tab1);; 
+      }
+      if (!input.value.match(/^[0-9]+$/g)) {
+        input.classList.add('error')
+      } else {
+        input.classList.remove('error') ;
+      }
+    });
+  });
+ // сделал валидацию, фильтр не получается  
+ return  shipCard(ships, tab1);
 }   
--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -1,6 +1,35 @@
 export default () => {
-  const cards = document.querySelector(".contentWrapper");
-  console.log(cards);
+  const cards = document.querySelectorAll('.main-card');
+  cards.forEach((el) => el.addEventListener('click', (e) => {
+      const target = e.target;
+      const currentCard = target.closest('.main-card');
+      const popup = document.getElementById("popup");
+      const popupContent = document.getElementById("popup-content");
+      popup.classList.add("visible")
+      const makeClone = (e) => {
+          if (e === null) {
+              return;
+          }
+          return e.cloneNode(true);
+      }
+      const clone = makeClone(currentCard);
+      const close = document.createElement('a');
+      close.textContent = "X";
+      close.setAttribute('href', "#");
+      close.classList.add('close');
+      clone.prepend(close);
+      popupContent.appendChild(clone);
 
-  }
+      close.addEventListener("click", () => {
+          popup.classList.remove("visible");
+          popupContent.textContent = "";
 
+      })
+
+      popupContent.addEventListener("click", () => {
+          popup.classList.remove("visible");
+          popupContent.textContent = "";
+
+      });
+  }))
+}
