db6fe90871ba71f060a624e86016f37fe06b94fb
Alex <alex-m-v@yandex.ru>
2021-04-16 13:52:12 +0300

    Update form.js

--- src/js/form.js
+++ src/js/form.js
@@ -6,6 +6,7 @@ function sendForm() {
   let arr = [];
   inputText.forEach(item => {
     if (item.value !== '') {
+      item.style.border = "2px solid black";
        arr.push(item.value);
     } else {
       item.style.border = "2px solid red";
