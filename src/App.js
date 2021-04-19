bb834179ef3820bddb38363feb20fbf6c005fd69
apollon313131 <apollo-bel@mail.ru>
2021-04-20 01:54:40 +0300

    add redrering if

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Modal from "./components/modal/Modal";
 import React, { useState } from 'react';
 
 const App = () => {
-  const [modalActive, setModalActive] = useState(true);
+  const [modalActive, setModalActive] = useState(false); 
+  const [name, setName] = useState('Вова'); 
+  const [checked, setChecked] = useState(false);
+  let box= {};
+  if(checked) {
+    box = <input type="checkbox" name="yes" id="radio2"/>
+  } else {
+    box = ''
+  }
+  const changeName = () => {
+    setName('Новый Вова')
+  }
   return (
     <div className="App">
         <h1 style={{color: "green"}}>Здесь могла быть Ваша реклама 1111</h1>
@@ -12,9 +23,17 @@ const App = () => {
           <button className='open-btn' onClick={() => setModalActive(true)}>Открыть модальное окно</button>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae aliquid autem in ducimus perferendis, eos quasi officia explicabo provident, error quo possimus quam ea voluptate exercitationem ex dicta ut accusamus?</p>
         </main>
-          <Modal active={modalActive} setActive={setModalActive}>
-            
+          <Modal active={modalActive} setActive={setModalActive} name={name}>
+
           </Modal>
+          <div>
+          <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} /> Выбрать
+	      	<div>{box}</div>
+          </div>
+
+          <button onClick={changeName}>
+            Изменить имя
+          </button>
     </div>
   );
 }
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import "./modal.css"
+import React from "react";
+import "./modal.css";
 
-const Modal = ({ active, setActive }) => {
-    return (
-        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
-            <div className={active ? "modal__content active" : "modal__content"} onClick={e => e.stopPropagation()}>
-                <p> Заявка на участие в рейде на Испанский золотой флот</p>
-                <form action="" className='formCapitan'>
-                    <input type="text" className='name' placeholder='Имя капитана' />
-                    <input type="text" className='name' placeholder='Имя корабля' />
-                    <div className='checkbox'>
-                        <input type="checkbox" name='no' className='check' /> Нет
-                    <input type="checkbox" name='yes' className='check' /> Да
-                </div>
-                    <div className='button__div'>
-                        <button>Сбросить</button>
-                        <button>Отправить в плавание</button>
-                    </div>
-                </form>
+const Modal = ({ active, setActive, name }) => {
+  return (
+    <div
+      className={active ? "modal active" : "modal"}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={active ? "modal__content active" : "modal__content"}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <p> Заявка на участие в рейде на Испанский золотой флот</p>
+        <form action="" className="formCapitan">
+          <input
+            type="text"
+            className="name"
+            placeholder="Имя капитана"
+            value={name}
+          />
+          <input type="text" className="name" placeholder="Имя корабля" />
+          <div className="checkbox">
+            <div className="check1">
+            
+              <input type="checkbox" name="no" className="radio" id='radio'/>
+              <label htmlFor="radio" className='mycheckbox'>Да</label>
+
             </div>
-        </div>
-    );
+
+            <br/>
+            <div className="check1">
+              <input type="checkbox" name="yes" className="radio" id='radio2' />
+              <label htmlFor="radio2">Да </label>
+            </div>
+          </div>
+          <div className="button__div">
+            <button>Сбросить</button>
+            <button>Отправить в плавание</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
+
+function greet(props) {
+    
+}
+
 
 export default Modal;
